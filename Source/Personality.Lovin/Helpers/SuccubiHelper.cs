b48afd7d4fd12edf6ad31f1157c4eee8d1fe17f5
effervescent000 <kislio01@gmail.com>
2024-02-25 10:42:35 -0500

    get aging multis working

--- Source/Personality.Lovin/Helpers/SuccubiHelper.cs
+++ Source/Personality.Lovin/Helpers/SuccubiHelper.cs
@@ -10,6 +10,13 @@ namespace Personality.Lovin;
 
 public static class SuccubiHelper
 {
+    public static SimpleCurve SuccubiAgingCurve = new()
+    {
+        new CurvePoint(1f, -1f),
+        new CurvePoint(0.5f, 0f),
+        new CurvePoint(0f, 10f),
+    };
+
     public static void OffsetVitality(Pawn pawn, float amount)
     {
         Gene_Resource vitality = (Gene_Resource)pawn.genes.GetGene(LovinDefOf.PP_VitalityRoot);
@@ -17,4 +24,21 @@ public static class SuccubiHelper
     }
 
     public static bool IsLoveFeeder(this Pawn pawn) => pawn.genes.HasGene(LovinDefOf.PP_LoveFeeder);
+
+    public static float GetRapidAgingMultiplier(Pawn pawn)
+    {
+        var hediff = pawn.health.hediffSet.GetFirstHediffOfDef(LovinDefOf.PP_VitalityLost);
+        if (hediff == null)
+        {
+            return 1f;
+        }
+
+        var comp = hediff.TryGetComp<HediffComp_RapidAging>();
+        SimpleCurve agingCurve = new()
+        {
+            new CurvePoint(0f, 1f),
+            new CurvePoint(1f, comp.Props.multiplierAtMax)
+        };
+        return agingCurve.Evaluate(hediff.Severity);
+    }
 }
