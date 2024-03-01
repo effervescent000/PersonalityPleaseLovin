df602e808670b11dc0270f7c2b1ff8da35fff5eb
effervescent000 <kislio01@gmail.com>
2024-03-01 18:22:10 -0500

    add genes

---
+++ Source/Personality.Lovin/DamageWorkers/DamageWorker_Disorient.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Personality.Lovin;
+
+public class DamageWorker_Disorient : DamageWorker
+{
+    public override DamageResult Apply(DamageInfo dinfo, Thing victim)
+    {
+        if (victim.def.defName == "Human" && victim != dinfo.Instigator)
+        {
+            CompAbilityEffect_GiveMentalState.TryGiveMentalStateWithDuration(MentalStateDefOf.Wander_Psychotic, (Pawn)victim, LovinAbilityDefOf.PP_DisorientAbility, null, (Pawn)dinfo.Instigator);
+        }
+
+        return new DamageResult();
+    }
+}
---
+++ Source/Personality.Lovin/DefOfs/LovinAbilityDefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+
+namespace Personality.Lovin;
+
+[DefOf]
+public static class LovinAbilityDefOf
+{
+    public static AbilityDef PP_SeduceAbility;
+    public static AbilityDef PP_DisorientAbility;
+
+    static LovinAbilityDefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(LovinAbilityDefOf));
+    }
+}
