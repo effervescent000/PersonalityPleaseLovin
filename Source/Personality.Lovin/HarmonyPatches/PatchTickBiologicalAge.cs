9306db957be654691bd6011a1bd91a5ff5df737e
effervescent000 <kislio01@gmail.com>
2024-03-03 10:20:32 -0500

    patch age tick so succubi can actually age in reverse

---
+++ Source/Personality.Lovin/HarmonyPatches/PatchTickBiologicalAge.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+using Verse;
+
+namespace Personality.Lovin.HarmonyPatches;
+
+[HarmonyPatch(typeof(Pawn_AgeTracker), "TickBiologicalAge")]
+public static class PatchTickBiologicalAge
+{
+    public static bool Prefix(int interval, Pawn_AgeTracker __instance, ref float ___progressToNextBiologicalTick, ref long ___ageBiologicalTicksInt, ref Pawn ___pawn)
+    {
+        if (!___pawn.genes?.HasGene(LovinDefOf.PP_VitalityRoot) ?? false)
+        {
+            return true;
+        }
+
+        float ticksThisCall = __instance.BiologicalTicksPerTick * (float)interval;
+        if (ticksThisCall > 0f) return true;
+
+        int num = Mathf.FloorToInt(ticksThisCall);
+        Log.Message(num.ToString());
+
+        ___ageBiologicalTicksInt += num;
+
+        return false;
+    }
+}
