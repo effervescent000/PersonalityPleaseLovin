e1341ebf0f764ed0bcae116351feaf1ed7b2607c
effervescent000 <kislio01@gmail.com>
2024-03-03 19:20:30 -0500

    make a primary jobDriver with subclasses

---
+++ Source/Personality.Lovin/Jobs/JobDriver_DoLovinIntimate.cs
@@ -0,0 +1,11 @@
+﻿namespace Personality.Lovin;
+
+public class JobDriver_DoLovinIntimate : JobDriver_DoLovin
+{
+    protected override void JobSpecificSetup()
+    {
+        ticksBase = GeneralHelper.GetHourBasedDuration(1f);
+        ticksForEnhancer = GeneralHelper.GetHourBasedDuration(2f);
+        context = LovinContext.Intimate;
+    }
+}
